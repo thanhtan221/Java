c6042e511b62b1f2b579298119fac988986a1e57
unknown <phanvanthanhtan1234@gmail.com>
2021-10-07 15:07:32 +0700

    KHAIBAOVASUDUNGBIEN.java

---
+++ KHAIBAOVASUDUNGBIEN.java
@@ -0,0 +1,21 @@
+package vn.codegym.vn;
+
+public class KHAIBAOVASUDUNGBIEN {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 't';
+        long l = 1000000000000000l;//chứa được nhiều số nguyên
+        String s = "Hà Nội";
+//        final Hằng phải viết hoa Toàn Bộ
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+
+    }
+}
