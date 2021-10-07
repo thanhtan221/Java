fe11b44fddd57d007699c337dd471c5803877d0b
unknown <phanvanthanhtan1234@gmail.com>
2021-10-07 15:14:53 +0700

    TínhoNgayTrongThang.java

---
+++ TinhSoNgayTrongThang.java
@@ -0,0 +1,37 @@
+package vn.codegym.vn;
+
+import java.util.Scanner;
+
+public class TinhSoNgayTrongThang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of days you want to find the month : ");
+        int month = sc.nextInt();
+        String daysInMonth;
+        switch (month) {
+            case 2:
+                daysInMonth = "28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = "30";
+                break;
+            default:
+                daysInMonth = "";
+        }
+        if (daysInMonth != "")
+            System.out.printf("The month '̀%d' has '%s' days!",month,daysInMonth);
+        else System.out.println("Invalid input");
+    }
+}
