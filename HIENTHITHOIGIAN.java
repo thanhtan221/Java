894c3c330d3d3007df06bb4127f9889ca9570197
unknown <phanvanthanhtan1234@gmail.com>
2021-10-07 15:06:21 +0700

    HIENTHITHOIGIAN.java

---
+++ HIENTHITHOIGIAN.java
@@ -0,0 +1,10 @@
+package vn.codegym.vn;
+
+import java.util.Date;
+
+public class HIENTHITHOIGIAN {
+    public static void main(String[] args) {
+        Date now = new Date();// Lấy Thời gian Máy Tính
+        System.out.println("now is" + now);
+    }
+}
